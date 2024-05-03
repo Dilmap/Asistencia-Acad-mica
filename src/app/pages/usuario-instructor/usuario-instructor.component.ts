d7e06c89c2224772e029304e1c17f4d106d1f7ba
DieguitoLOL <diegoabdalaflorez@gmail.com>
2024-05-03 08:56:34 -0500

    Realizado pagina del usuario

---
+++ src/app/pages/usuario-instructor/usuario-instructor.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-instructor',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario-instructor.component.html',
+  styleUrl: './usuario-instructor.component.css'
+})
+export class UsuarioInstructorComponent {
+  // function guardar() {
+  //   alert('Asistencia revisada');
+  // }
+  // document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
+  //     this.querySelector('.custom-select').classList.toggle('opened');
+  //   });
+
+  //   for (const option of document.querySelectorAll(".custom-option")) {
+  //     option.addEventListener('click', function() {
+  //       if (!this.classList.contains('selected')) {
+  //         this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+  //         this.classList.add('selected');
+  //         this.closest('.custom-select').querySelector('.custom-select-trigger').textContent = this.textContent;
+  //       }
+  //     })
+  //   }
+}
