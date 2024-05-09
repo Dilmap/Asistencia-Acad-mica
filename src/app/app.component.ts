9b40b39565ec0a2795322ac4b067c0b6e905c496
murallas <murallas512bv@gmail.com>
2024-05-09 16:03:12 -0500

    Iniciodiseñoderegistroasistencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { HomeComponent } from './pages/home/home.component';
+import { UsuarioInstructorComponent } from './pages/usuario-instructor/usuario-instructor.component';
+import { RegistroAsistenciaComponent } from './pages/registro-asistencia/registro-asistencia.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegistroComponent,HomeComponent],
+  imports: [RouterOutlet, LoginComponent, RegistroComponent,HomeComponent,RegistroAsistenciaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
