763edd297f8a1480788d6ef5b28917de0fab65d7
Gabrielrmr <murallas512bv@gmail.com>
2024-05-17 08:18:13 -0500

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegistroAsistenciaComponent } from './pages/registro-asistencia/registro-asistencia.component';
 import { VerificarAprendizComponent } from './pages/verificar-aprendiz/verificar-aprendiz.component';
 import { ReporteDiarioComponent } from './pages/reporte-diario/reporte-diario.component';
+import { ReportesComponent } from './pages/reportes/reportes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegistroComponent,HomeComponent,RegistroAsistenciaComponent,VerificarAprendizComponent,ReporteDiarioComponent],
+  imports: [RouterOutlet, LoginComponent, RegistroComponent,HomeComponent,RegistroAsistenciaComponent,VerificarAprendizComponent,ReporteDiarioComponent,ReportesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
