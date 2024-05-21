ef2761a1c41bcb384bd7ac5df12cd56045a8fa6f
Gabrielrmr <murallas512bv@gmail.com>
2024-05-21 15:58:10 -0500

    C8

--- src/app/pages/registro-asistencia/registro-asistencia.component.ts
+++ src/app/pages/registro-asistencia/registro-asistencia.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registro-asistencia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './registro-asistencia.component.html',
   styleUrl: './registro-asistencia.component.css'
 })
 export class RegistroAsistenciaComponent {
+  isDivVisible: boolean = false;
 
+  toggleDiv() {
+    this.isDivVisible = !this.isDivVisible;
+  }
 }
