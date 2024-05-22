574cd9aa73458cf8d19919dc64f8a36804774ce0
Gabrielrmr <murallas512bv@gmail.com>
2024-05-22 10:37:29 -0500

    editarcomponet

--- src/app/pages/registro-asistencia/registro-asistencia.component.ts
+++ src/app/pages/registro-asistencia/registro-asistencia.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActualizarInstructorComponent } from '../actualizar-instructor/actualizar-instructor.component';
 
 @Component({
   selector: 'app-registro-asistencia',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ActualizarInstructorComponent],
   templateUrl: './registro-asistencia.component.html',
   styleUrl: './registro-asistencia.component.css'
 })
