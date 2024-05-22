82713b8d06d62603e22ccf632adfee8ae21c47a8
DieguitoLOL <diegoabdalaflorez@gmail.com>
2024-05-22 10:05:32 -0500

    Agregando la logica del instructor

--- src/app/pages/registro-asistencia/registro-asistencia.component.ts
+++ src/app/pages/registro-asistencia/registro-asistencia.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { InstructorService } from './../../shared/instructor_crud/instructor.service';
+import { Observable } from 'rxjs';
+import { instructorModel } from './../../shared/instructor_crud/instructor.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registro-asistencia',
@@ -7,6 +10,12 @@ import { Component } from '@angular/core';
   templateUrl: './registro-asistencia.component.html',
   styleUrl: './registro-asistencia.component.css'
 })
-export class RegistroAsistenciaComponent {
+export class RegistroAsistenciaComponent implements OnInit{
+  instructores: Observable<instructorModel[]> | undefined
 
+  constructor(private instructorService: InstructorService){ }
+
+  ngOnInit(){
+    this.instructores = this.instructorService.obtenerInstructores();
+  }
 }
---
+++ src/app/shared/instructor_crud/instructor.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { instructorModel } from './instructor.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstructorService {
+  DATABASE = 'http://localhost:3000'
+  constructor(private http: HttpClient) { }
+
+  obtenerInstructores(){
+    return this.http.get<instructorModel[]>(this.DATABASE + '/instructores');
+  }
+
+  obtenerInstructor(numeroDocumento: string){
+    return this.http.get<instructorModel[]>(`${this.DATABASE}/instructores/${numeroDocumento}`);
+  }
+
+  agregarInstructor(instructor: instructorModel){
+    return this.http.post<string>(`${this.DATABASE}/instructores`, instructor);
+  }
+
+  actualizarUsuario(instructor: instructorModel){
+    return this.http.put<string>(`${this.DATABASE}/instructors/actualizar/${instructor.numeroDocumento}`, instructor);
+  }
+
+  borrarUsuario(numeroDocumento: string){
+    return this.http.delete<string>(`${this.DATABASE}/instructors/borrar/${numeroDocumento}`);
+  }
+}
