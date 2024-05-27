77971adafeae050de2214a772997c290b43ffba5
Gabrielrmr <murallas512bv@gmail.com>
2024-05-27 09:08:55 -0500

    c21

--- src/app/pages/reportes/reportes.component.ts
+++ src/app/pages/reportes/reportes.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
 export class ReportesComponent {
+  arg:any
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, interactionPlugin],
+    events: [
+      // Tus eventos aquí
+    ],
 
+  };
 }
