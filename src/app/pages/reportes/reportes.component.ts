47fc643f35b67c23c06a953052c5b8c273c1c72c
DieguitoLOL <diegoabdalaflorez@gmail.com>
2024-06-04 11:48:44 -0500

    Terminando la logica de guardar el instructor 2

--- src/app/pages/reportes/reportes.component.ts
+++ src/app/pages/reportes/reportes.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
+
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [FullCalendarModule],
+  imports: [],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
 export class ReportesComponent {
-  arg:any
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin, interactionPlugin],
-    events: [
-      // Tus eventos aquí
-    ],
 
-  };
 }
