11142ff1ab97d078743b926c3ca7f54371fd266a
Gabrielrmr <murallas512bv@gmail.com>
2024-06-13 09:11:38 -0500

    c12

--- src/app/pages/registro-asistencia/registro-asistencia.component.ts
+++ src/app/pages/registro-asistencia/registro-asistencia.component.ts
@@ -1,19 +1,21 @@
 import { FichaService } from './../../shared/Ficha_crud/ficha.service';
 import { CommonModule } from '@angular/common';
 import { ActualizarInstructorComponent } from '../actualizar-instructor/actualizar-instructor.component';
-import { Component, OnInit,viewChild } from '@angular/core';
+import { Component, OnInit,Input,viewChild,Output,EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { CompetenciaService } from '../../shared/competencia_crud/competencia.service';
 import { GuardarAsistenciaComponent } from '../guardar-asistencia/guardar-asistencia.component';
 import { CompetenciaModel } from '../../shared/competencia_crud/competencias.model';
 import { FichaModel } from '../../shared/Ficha_crud/ficha.model';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @Component({
   selector: 'app-registro-asistencia',
   standalone: true,
-  imports: [CommonModule,ActualizarInstructorComponent, FormsModule, GuardarAsistenciaComponent,MatPaginatorModule],
+  imports: [CommonModule,ActualizarInstructorComponent, FormsModule, GuardarAsistenciaComponent,MatSelectModule,MatFormFieldModule,MatInputModule],
   templateUrl: './registro-asistencia.component.html',
   styleUrl: './registro-asistencia.component.css'
 })
@@ -21,9 +23,27 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 export class RegistroAsistenciaComponent implements OnInit{
   isDivVisible: boolean = false;
 
+
+  @Input() competencias2: Observable<{ nombre: string }[]> = new Observable();
+  @Input() placeholder: string = 'Select an option';
+  @Output() selectionChange = new EventEmitter<{ nombre: string }>();
+
+  isDropdownOpen = false;
+  selectedOption: { nombre: string } | null = null;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectOption(option: { nombre: string }) {
+    this.selectedOption = option;
+    this.selectionChange.emit(option);
+    this.isDropdownOpen = false; // Close the dropdown after selection
+  }
+
   competencias: Observable<CompetenciaModel[]> | undefined;
   fichaProgramas: Observable<FichaModel[]> | undefined;
-
+  selectedCompetencia: CompetenciaModel | null = null;
   constructor(
     private competenciaService: CompetenciaService,
     private fichaService: FichaService,
